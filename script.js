882c2a38905a67ae8b4b6476d928ce029536830c
Arief Ardi <34484201+ariefardi@users.noreply.github.com>
2017-12-23 23:11:28 +0700

    update tugas baru/scriptbaru

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-var bintang = prompt('Karakter kosong')
-for(index1=1; index1 < 11; index1 = index1+1){
-  for(index2=0; index2 >= index1; index2 = index2+1){
- }
- console.log(bintang);
- var bintang = prompt('Karakter kosong')
+//tugas 1
+var total=0;
+for(var i=1; i<101;i=i+1){
+	if (i%2===1) {
+		total = total+i;
+	}
+	else
+		if (i%2===0) {
+			total = total-i;
+		}
+}
+
+//tugas 2
+var pagar = prompt('');
+for(index1=1; index1>=10;index1=index1+1){
+	for(index2=0; index2>=10;index2=index2+1){
+		pagar= pagar + '#';
+	}
+	console.log(pagar)
+	pagar = prompt('')
+}
+
+//tugas 3
+var bintagg = prompt('');
+for(index1=1;index1>=10;index1+index1+1){
+	for(index2=0;index2>=10){
+		bintang= bintang+('*')
+	}
+	console.log(bintang)
+	bintang =prompt('')
 }
