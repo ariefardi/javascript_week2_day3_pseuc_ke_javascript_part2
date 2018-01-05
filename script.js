f4716ed89f03325326b8543b631ba1bca5d4a522
Arief Ardi <34484201+ariefardi@users.noreply.github.com>
2018-01-05 22:50:26 +0700

    update terbbaru gak error

--- script.js
+++ script.js
@@ -11,21 +11,24 @@ for(var i=1; i<101;i=i+1){
 }
 
 //tugas 2
-var pagar = prompt('');
-for(index1=1; index1>=10;index1=index1+1){
-	for(index2=0; index2>=10;index2=index2+1){
-		pagar= pagar + '#';
-	}
-	console.log(pagar)
-	pagar = prompt('')
+var pagar  = '';
+for(index1=1;index1<11;index1++){
+  for(index2=0;index2<11;index2++){
+    pagar = pagar + '#'
+  }
+  console.log(pagar);
+  var pagar = '';
 }
 
+
 //tugas 3
-var bintagg = prompt('');
-for(index1=1;index1>=10;index1+index1+1){
-	for(index2=0;index2>=10){
-		bintang= bintang+('*')
-	}
-	console.log(bintang)
-	bintang =prompt('')
+var bintang = '';
+for(index1=1;index1<11;index1++){
+  for(index2=0;index2<11;index2++){
+    bintang=bintang+'*'
+  }
+  console.log(bintang);
+  var bintang = '';
 }
+
+
